df6ac76815c9cd7639be0c4b7cdce1b8bda0999e
RDCLder <riy2@case.edu>
2018-11-19 14:55:15 -0600

    Steps 7 + Bonus + Extra Additions

--- heroRPG.py
+++ heroRPG.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
-# In this simple RPG game, the hero fights the goblin. He has the options to:
+# In this simple RPG game, the hero fights an enemy. He has the options to:
 
-# 1. fight goblin
-# 2. do nothing - in which case the goblin will attack him anyway
+# 1. fight enemy
+# 2. do nothing - in which case the enemy will attack him anyway
 # 3. flee
+
+#-------------------------------------------------------------------------------------#
+
+# Initialize
+
+from random import randint
 
 class Character:
 
@@ -14,8 +20,9 @@ class Character:
         self.power = power
 
     def attack(self, target):
-        target.health -= self.power
-        print(f"You do {self.power} damage to the goblin.")
+        damage = randint(0, self.power)
+        target.health -= damage
+        print(f"The {self.name} does {damage} damage to the {target.name}.")
         if target.health <= 0:
             print(f"The {target.name} is dead.")
 
@@ -24,38 +31,48 @@ class Character:
             return True
     
     def status(self):
-        print(f"You have {self.health} health and {self.power} power.")
+        print(f"The {self.name} has {self.health} health and {self.power} power.")
+
+
+goblin = Character("Goblin", 10, 2)
+zombie = Character("Zombie", 9000, 1)
+dragon = Character("Dragon", 1000000, 100)
+enemies = [zombie, goblin, dragon]
+
+#-------------------------------------------------------------------------------------#
+
+# Main Program
+
+yourName = input("What is your name, adventurer?\n")
+adventurer = Character(yourName, 40, 5)
 
 def main():
-    hero_health = 10
-    hero_power = 5
-    goblin_health = 6
-    goblin_power = 2
-    hero = Character("Hero", hero_health, hero_power)
-    goblin = Character("Goblin", goblin_health, goblin_power)
+    
+    enemy = enemies[randint(0, len(enemies) - 1)]
+    print(f"\n{adventurer.name} encounters a wild {enemy.name}!")
 
-    while goblin.alive() and hero.alive():
-        hero.status()
-        goblin.status()
+    while enemy.alive() and adventurer.alive():
+        adventurer.status()
+        enemy.status()
         print("\nWhat do you want to do?")
-        print("1. fight goblin")
+        print(f"1. fight {enemy.name}")
         print("2. do nothing")
         print("3. flee")
         print("> ", end=' ')
         raw_input = input()
         if raw_input == "1":
-            # Hero attacks goblin
-            hero.attack(goblin)
+            adventurer.attack(enemy)
         elif raw_input == "2":
             pass
         elif raw_input == "3":
             print("Goodbye.")
             break
         else:
-            print("Invalid input {}".format(raw_input))
+            print(f"Invalid input {raw_input}")
 
-        if goblin.health > 0:
-            # Goblin attacks hero
-            goblin.attack(hero)
+        if enemy.health > 0:
+            enemy.attack(adventurer)
 
 main()
+
+#-------------------------------------------------------------------------------------#
